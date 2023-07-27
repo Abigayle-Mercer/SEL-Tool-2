fa30daee1d6028b4060971f667a89d6f87111421
Abigayle Mercer <abigayle.mercer@gmail.com>
2023-07-26 18:27:18 -0700

    Deploying to GitHub Pages

--- src/Components/EmailForm/EmailForm.js
+++ src/Components/EmailForm/EmailForm.js
@@ -7,15 +7,13 @@ function EmailForm(props) {
         <div className="modal">
           <div onClick={props.toggleModal} className="overlay"></div>
           <div className="modal-content">
-            <h2>Email Form</h2>
-            <div>
-              <label className="lab">Name</label>
-              <input type="text" id="name" name="name" />
+            <h2>Save PDF</h2>
+            
+            <div className="name">
+              <label className="lab">Please enter a name for this file: </label>
+              <input classname="in" type="text" id="name" name="name" />
             </div>
-            <div>
-              <label className="lab">Email</label>
-              <input type="email" id="user_email" name="user_email" />
-            </div>
+            
             <input className="send" type="submit" value="send"></input>
           </div>
         </div>
--- src/Pages/Wheel.js
+++ src/Pages/Wheel.js
@@ -1,66 +1,66 @@
 import React from "react";
-import {useState} from "react";
+import { useState } from "react";
 import Pie from "../Components/Pie/Pie.js";
 import "./Wheel.css";
 import "react-quill/dist/quill.snow.css";
-import emailjs from'emailjs-com';
+import emailjs from "emailjs-com";
 import html2canvas from "html2canvas";
 import jsPDF from "jspdf";
 import ReflectionTextBox from "../Components/ReflectionTextBox/ReflectionTextBox.js";
 import EmailForm from "../Components/EmailForm/EmailForm.js";
-
+import { AbortedDeferredError } from "react-router-dom";
 
 function Wheel(props) {
-  function sendEmail(e) {
+  function savePDF(e) {
     e.preventDefault();
-    let name = document.getElementById("name").value;
-    let user_email = document.getElementById("user_email").value;
-    let Q1 = document.getElementById("Q1").value;
-    let Q2 = document.getElementById("Q2").value;
-    let Q3 = document.getElementById("Q3").value;
-    let Q4 = document.getElementById("Q4").value;
-    let Q5 = document.getElementById("Q5").value;
-    let Q6 = document.getElementById("Q6").value;
+    setModal(false);
+    // let name = document.getElementById("name").value;
+    // let user_email = document.getElementById("user_email").value;
+    // let Q1 = document.getElementById("Q1").value;
+    // let Q2 = document.getElementById("Q2").value;
+    // let Q3 = document.getElementById("Q3").value;
+    // let Q4 = document.getElementById("Q4").value;
+    // let Q5 = document.getElementById("Q5").value;
+    // let Q6 = document.getElementById("Q6").value;
+    let DocName = document.getElementById("name").value;
 
+    const totalPageHeight = document.documentElement.scrollHeight;
+    const totalPageWidth = document.documentElement.scrollWidth;
     const input = document.getElementById("outermost");
+    const input2 = document.getElementById("reflections")
+    const pdf = new jsPDF("p", "mm", "a4");
+
     html2canvas(input, {
       logging: true,
       letterRendering: 1,
       useCORS: true,
+      scrollY: -window.scrollY, // Set scrollY to the top to capture the full page
+      scrollX: -window.scrollX,
+      windowHeight: totalPageHeight, // Set the canvas height to the total page height
+      windowWidth: totalPageWidth,
     }).then((canvas) => {
       const imgWidth = 208;
       const imgHeight = (canvas.height * imgWidth) / canvas.width;
       const imgData = canvas.toDataURL("img/png");
-      const pdf = new jsPDF("p", "mm", "a4");
       pdf.addImage(imgData, "PNG", 0, 0, imgWidth, imgHeight);
-
-      var contactParams = {
-        name: name,
-        user_email: user_email,
-        Q1: Q1,
-        Q2: Q2,
-        Q3: Q3,
-        Q4: Q4,
-        Q5: Q5,
-        Q6: Q6,
-        
-      };
-
-      emailjs
-        .send(
-          "service_anbmlu1",
-          "template_fte9xmb",
-          contactParams,
-          "-wdPkFamHwHCF0jMZ"
-        )
-        .then((res) => {
-          console.log(res);
-        })
-        .catch((err) => console.log(err));
-      setModal(false);
+    });
+       html2canvas(input2, {
+      logging: true,
+      letterRendering: 1,
+      useCORS: true,
+      scrollY: -window.scrollY, // Set scrollY to the top to capture the full page
+      scrollX: -window.scrollX,
+      windowHeight: totalPageHeight, // Set the canvas height to the total page height
+      windowWidth: totalPageWidth,
+    }).then((canvas) => {
+      const imgWidth = 208;
+      const imgHeight = (canvas.height * imgWidth) / canvas.width;
+      const imgData = canvas.toDataURL("img/png");
+      pdf.addPage();
+      pdf.addImage(imgData, "PNG", 0, 0, imgWidth, imgHeight);
+      pdf.save(`${DocName}.pdf`);
     });
   }
-  
 
   const [modal, setModal] = useState(false);
 
@@ -74,36 +74,11 @@ function Wheel(props) {
     document.body.classList.remove("active-modal");
   }
 
-
-
-
   return (
-    <div>
-      <div id="outermost" className="outermost">
-        <div className="container1">
-          <div className="Wrapper">
-            <div>
-              <p className="l1">Mindset</p>
-              <p className="l2">Critical Thinking</p>
-            </div>
-
-            <div className="column">
-              <p className="t1">Personal Practices Reflection</p>
-              <p className="l6">Sensory-Motor Integration</p>
-              <Pie id="pie" slices={props.slices} />
-              <div className="bottomlabels">
-                <p className="l5">Connection & Identity</p>
-                <p className="l5">Collaboration</p>
-              </div>
-            </div>
-
-            <div>
-              <p className="l3">Insight</p>
-              <p className="l4">Regulation</p>
-            </div>
-          </div>
-
-          <div className="container2">
+    <div id="WholePage">
+      <div id="pdfContent">
+        <div id="outermost" className="outermost">
+          <div className="container1">
             <div className="Wrapper">
               <div>
                 <p className="l1">Mindset</p>
@@ -111,9 +86,9 @@ function Wheel(props) {
               </div>
 
               <div className="column">
-                <p className="t2">Teaching Practifces Reflection</p>
+                <p className="t1">Personal Practices Reflection</p>
                 <p className="l6">Sensory-Motor Integration</p>
-                <Pie slices={props.slices2} />
+                <Pie id="pie" slices={props.slices} />
                 <div className="bottomlabels">
                   <p className="l5">Connection & Identity</p>
                   <p className="l5">Collaboration</p>
@@ -125,19 +100,46 @@ function Wheel(props) {
                 <p className="l4">Regulation</p>
               </div>
             </div>
+
+            <div className="container2">
+              <div className="Wrapper">
+                <div>
+                  <p className="l1">Mindset</p>
+                  <p className="l2">Critical Thinking</p>
+                </div>
+
+                <div className="column">
+                  <p className="t2">Teaching Practifces Reflection</p>
+                  <p className="l6">Sensory-Motor Integration</p>
+                  <Pie slices={props.slices2} />
+                  <div className="bottomlabels">
+                    <p className="l5">Connection & Identity</p>
+                    <p className="l5">Collaboration</p>
+                  </div>
+                </div>
+
+                <div>
+                  <p className="l3">Insight</p>
+                  <p className="l4">Regulation</p>
+                </div>
+              </div>
+            </div>
           </div>
+        </div>
+
+        <div id="reflections" className="reflections">
+          <ReflectionTextBox className="reflections"></ReflectionTextBox>
         </div>
       </div>
 
-      <ReflectionTextBox></ReflectionTextBox>
-
       <button className="btn-modal" onClick={toggleModal}>
-        Want a copy of your responses?
+        Want a PDF of your responses?
       </button>
+      {/* <button classname="btn-modal" onClick={sendEmail}>
+        Want a PDF of your responses?
+      </button> */}
 
-      {modal && (
-        <EmailForm toggleModal={toggleModal} sendEmail={sendEmail}/>
-      )}
+      {modal && <EmailForm toggleModal={toggleModal} sendEmail={savePDF} />}
     </div>
   );
 }
